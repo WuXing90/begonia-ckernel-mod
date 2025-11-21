31e688637fcb7b41d26d99e519bf1955ab83f5f0
F640 <92260436+F640@users.noreply.github.com>
2025-11-22 00:41:57 +0700

    modularize script and changed workflow filename

--- kernel-scripts/plant-ksu-defconfig.sh
+++ kernel-scripts/plant-ksu-defconfig.sh
@@ -3,18 +3,25 @@
 set -e
 export DEFCONFIG="$PWD/arch/arm64/configs/begonia_user_defconfig"
 
-echo '# CONFIG_KSU is not set' >> $DEFCONFIG
-echo '# CONFIG_KSU_DEBUG is not set' >> $DEFCONFIG
+# comment means left to config defaults
+declare -a add_feature_flags=(
+    "CONFIG_KSU"
+    "CONFIG_KSU_DEBUG"
+    # KSUN and SukiSU specific
+    # "CONFIG_KSU_ALLOWLIST_WORKAROUND"
+    # KernelSU-Next specific
+    "CONFIG_KSU_KPROBES_HOOK"
+    # "CONFIG_KSU_LSM_SECURITY_HOOKS"
+    # SukiSU specific
+    "CONFIG_KPM"
+    "CONFIG_KSU_MANUAL_HOOK"
+    # "CONFIG_KSU_MANUAL_SU"
+    # dropped SukiSU-specific options
+    # "CONFIG_KSU_MULTI_MANAGER_SUPPORT"
+)
 
-# KSUN and SukiSU specific
-# echo '# CONFIG_KSU_ALLOWLIST_WORKAROUND is not set' >> $DEFCONFIG
-
-# KernelSU-Next specific
-echo '# CONFIG_KSU_KPROBES_HOOK is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_LSM_SECURITY_HOOKS is not set' >> $DEFCONFIG
-
-# SukiSU specific
-echo '# CONFIG_KPM is not set' >> $DEFCONFIG
-echo '# CONFIG_KSU_MANUAL_HOOK is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_MANUAL_SU is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_MULTI_MANAGER_SUPPORT is not set' >> $DEFCONFIG
+# add features logic
+for CONFIG in "${add_feature_flags[@]}"
+do
+   echo "# $CONFIG is not set" >> $DEV_DEFCONFIG
+done
--- kernel-scripts/plant-susfs-defconfig.sh
+++ kernel-scripts/plant-susfs-defconfig.sh
@@ -1,19 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 export DEFCONFIG="$PWD/arch/arm64/configs/begonia_user_defconfig"
 
-echo '# CONFIG_KSU_SUSFS is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_SUS_PATH is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_SUS_MOUNT is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_SUS_KSTAT is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_TRY_UMOUNT is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_SPOOF_UNAME is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_ENABLE_LOG is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_OPEN_REDIRECT is not set' >> $DEFCONFIG
-echo '# CONFIG_KSU_SUSFS_SUS_SU is not set' >> $DEFCONFIG
-# echo '# CONFIG_KSU_SUSFS_SUS_MAP is not set' >> $DEFCONFIG
+# comment means left to config defaults
+declare -a add_feature_flags=(
+    "CONFIG_KSU_SUSFS"
+    # "CONFIG_KSU_SUSFS_SUS_PATH"
+    # "CONFIG_KSU_SUSFS_SUS_MOUNT"
+    # "CONFIG_KSU_SUSFS_SUS_KSTAT"
+    # "CONFIG_KSU_SUSFS_TRY_UMOUNT"
+    # "CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT"
+    # "CONFIG_KSU_SUSFS_SPOOF_UNAME"
+    # "CONFIG_KSU_SUSFS_ENABLE_LOG"
+    # "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG"
+    # "CONFIG_KSU_SUSFS_OPEN_REDIRECT"
+    # "CONFIG_KSU_SUSFS_SUS_MAP"
+    # dropped (1.5.7)
+    # "CONFIG_KSU_SUSFS_SUS_OVERLAYFS"
+    # dropped (1.5.11)
+    # "CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT"
+    # dropped (2.0)
+    "CONFIG_KSU_SUSFS_SUS_SU"
+    # deprecated (2.0)
+    # "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT"
+    # "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT"
+)
+
+# add features logic
+for CONFIG in "${add_feature_flags[@]}"
+do
+   echo "# $CONFIG is not set" >> $DEV_DEFCONFIG
+done
