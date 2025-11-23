f6aae44f5e37220c7d8522525a290e1e05b5b162
F640 <92260436+F640@users.noreply.github.com>
2025-11-23 16:15:30 +0700

    refactor workflow and kvm script

--- kernel-scripts/enable-kvm-vfio.sh
+++ kernel-scripts/enable-kvm-vfio.sh
@@ -4,9 +4,17 @@ set -e
 export DEV_DEFCONFIG="$PWD/arch/arm64/configs/begonia_user_defconfig"
 
 # comment means left to config defaults
-declare -a add_enable_feature_flags=(
+declare -a enable_feature_flags=(
     # KVM
     "CONFIG_VIRTUALIZATION"
+
+    # VFIO
+    "CONFIG_VFIO"
+    "CONFIG_IOMMU_SUPPORT"
+)
+
+declare -a add_enable_feature_flags=(
+    # KVM
     "CONFIG_KVM"
 
     # VFIO
@@ -15,10 +23,15 @@ declare -a add_enable_feature_flags=(
     "CONFIG_VFIO_PCI"
     "CONFIG_VFIO_MDEV"
     "CONFIG_VFIO_MDEV_DEVICE"
-    "CONFIG_IOMMU_SUPPORT"
     "CONFIG_S390_CCW_IOMMU"
     "CONFIG_S390_AP_IOMMU"
 )
+
+# enable features logic
+for CONFIG in "${enable_feature_flags[@]}"
+do
+   sed -ri "s/^($CONFIG=.*|# $CONFIG is not set)/$CONFIG=y/" $DEV_DEFCONFIG
+done
 
 # add and enable features logic
 for CONFIG in "${add_enable_feature_flags[@]}"
@@ -26,8 +39,5 @@ do
    echo "$CONFIG=y" >> $DEV_DEFCONFIG
 done
 
-# enable VFIO
-sed -ri "s/^(CONFIG_VFIO=.*|# CONFIG_VFIO is not set)/CONFIG_VFIO=y/" $DEV_DEFCONFIG
-
 # ARM virtualization support
-echo "CONFIG_ARM64_VHE=1" >> $DEV_DEFCONFIG
+sed -ri "s/^(CONFIG_ARM64_VHE=.*|# CONFIG_ARM64_VHE is not set)/CONFIG_ARM64_VHE=1/" $DEV_DEFCONFIG
